9882ae9ac3d07a89f50fea70c0bb5e7ac8746b01
grace-go <eungo2@illinois.edu>
2021-06-11 06:58:09 -0500

    added tracker.py file

---
+++ tracker.py
@@ -0,0 +1,21 @@
+import json
+from plumbum import cli
+from questionary import prompt
+from pyfiglet import Figlet
+
+# A brief introduction about COVID-19 and its symptoms
+def symptoms():
+    return "hello"
+
+# Shows the total number of COVID-19 test cases in South Korea
+def total():
+    return "total_number"
+
+# Shows the total number of COVID-19 positive test cases in specific "region"
+def specific_area(region):
+    return region
+
+# Information about where to contact and test COVID-19
+def contact_info():
+    return "contact"
+
